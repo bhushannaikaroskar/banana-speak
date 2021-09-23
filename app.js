2483435d110a9d5af35a5e9bc9accdfa9cb902fc
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2021-09-24 02:44:39 +0530

    add api request and error handle

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ var buttonTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var textOutput = document.querySelector("#text-output");
 
+// Mock API
+// var apiURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var apiURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslateURL(text){
+    return encodeURI(apiURL+"?text="+text);
+}
+
+function errorHandling(error){
+    console.log("error handling ",error);
+    alert("Something went wrong. Try after sometime")
+}
+
 function clickEventHandler(){
-    textOutput.innerText = textInput.value;
+
+    var inputTxt = textInput.value;
+    
+    // textOutput.innerText = textInput.value;
+    fetch(getTranslateURL(inputTxt))
+    .then(response => response.json())
+    .then(json => {
+        console.log(json);
+        var translatedText = json.contents.translated;
+        textOutput.innerHTML = translatedText + inputTxt.value;
+    }).catch(errorHandling);
 }
 
 buttonTranslate.addEventListener("click",clickEventHandler);
+
